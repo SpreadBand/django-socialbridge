0577fccf03280475d43203f99228d1cf20966537
Guillaume Libersat <guillaume@spreadband.com>
2010-11-12 13:23:48 +0100

    intial structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-socialbridge',
+      version='0.1',
+      description='Bridge to social networks for Django',
+      author='Guillaume Libersat',
+      author_email='guillaume@spreadband.com',
+      url='https://github.com/SpreadBand/django-socialbridge',
+      packages=['socialbridge'],
+      )
